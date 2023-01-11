030ca3dbf7366f36ce05dc81bcd6cb8338d5d1cc
Anitha S <71218524+anithas0501@users.noreply.github.com>
2023-01-11 19:19:09 +0530

    Add files via upload

---
+++ Assignment09_ReverseString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class Assignment09_ReverseString
+    {
+        public static void reverseByWords(string str)
+        {
+
+            string[] words = str.Split(' ');
+            for (int i = 0; i < words.Length / 2; i++)
+            {
+                string ls = words[words.Length - 1 - i];
+                String fs = words[i];
+                words[words.Length - 1 - i] = fs;
+                words[i] = ls;
+            }
+
+            foreach (var item in words)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        public static void Main()
+        {
+
+            Console.Write("Enter String to reverse ");
+            string str = Console.ReadLine();
+            reverseByWords(str);
+
+        }
+    }
+}
